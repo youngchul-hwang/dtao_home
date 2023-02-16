a530f1fb7bd12ee6bf84e975a5e5e8df99ecb5a2
youngchul-hwang <89388094+youngchul-hwang@users.noreply.github.com>
2023-02-16 16:31:57 +0900

    Update PEXData.cpp

--- DTAO/Src/PEXData.cpp
+++ DTAO/Src/PEXData.cpp
@@ -26,16 +26,16 @@ namespace lve {
 
 	PEXItem::~PEXItem() {
 	}
-	
+
 	void PEXItem::print() {
 		printf("%s\n", this->name.c_str());
-		printf("Node1 name/layer#/x1,y1/Z-s,Z-e = %s / %d.%d / %.5f,%.5f / %.5f,%.5f\n", 
+		printf("Node1 name/layer#/x1,y1/Z-s,Z-e = %s / %d.%d / %.5f,%.5f / %.5f,%.5f\n",
 			this->node1.name.c_str(), this->node1.layer_number, this->node1.layer_datatype,
 			this->node1.x, this->node1.y, this->node1.z_start, this->node1.z_end);
 		printf("Node2 name/layer#/x1,y1/Z-s,Z-e = %s / %d.%d / %.5f,%.5f / %.5f,%.5f\n",
 			this->node1.name.c_str(), this->node1.layer_number, this->node1.layer_datatype,
 			this->node2.x, this->node2.y, this->node2.z_start, this->node2.z_end);
-}
+	}
 
 	/// <summary>
 	/// PEXResistor
@@ -64,7 +64,7 @@ namespace lve {
 			direction = RES_DIRECTION_HORIZONTAL;
 		}
 		else {
-			direction = RES_DIRECTION_VERTICAL;			
+			direction = RES_DIRECTION_VERTICAL;
 		}
 		//printf("#####Description = %s, ", description.c_str());
 		//if (direction == RES_DIRECTION_VERTICAL) printf("Vertical\n");
@@ -113,14 +113,14 @@ namespace lve {
 	}
 
 	void PEXResistorDataManager::loadData(const std::string file_path) {
-		if ( !std::filesystem::exists(file_path) ){
+		if (!std::filesystem::exists(file_path)) {
 			cerr << "Error! " << file_path << " is not exist!!\n";
 			return;
 		}
 
 		this->file_path = file_path;
 		rapidcsv::Document infile(file_path, rapidcsv::LabelParams(-1, -1));
-		
+
 		string item_name{}, node1_lvl{}, node2_lvl{};
 		node node1{}, node2{};
 		string description{};
@@ -128,7 +128,7 @@ namespace lve {
 
 		size_t line_count = infile.GetRowCount();
 		for (int i = 1; i < line_count; ++i) {
-			vector<string> line = infile.GetRow<string>(i); 
+			vector<string> line = infile.GetRow<string>(i);
 
 			item_name = line[PEXINFO_INDEX::PEXINFO_INDEX_NAME];
 			if (item_name[0] != 'R') continue;
@@ -140,30 +140,30 @@ namespace lve {
 			node1.z_end = stof(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE1_ZEND]);
 			node1.layer_number = stoi(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE1_LAYER_NUMBER]);
 			node1.layer_datatype = stoi(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE1_LAYER_DATATYPE]);
-			
+
 			node2.name = line[PEXINFO_INDEX::PEXINFO_INDEX_NODE2_NAME];
-			node2.x	= stof(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE2_X]);
-			node2.y	= stof(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE2_Y]);
+			node2.x = stof(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE2_X]);
+			node2.y = stof(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE2_Y]);
 			node2.z_start = stof(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE2_ZSTART]);
 			node2.z_end = stof(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE2_ZEND]);
 			node2.layer_number = stoi(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE2_LAYER_NUMBER]);
 			node2.layer_datatype = stoi(line[PEXINFO_INDEX::PEXINFO_INDEX_NODE2_LAYER_DATATYPE]);
 
 			value = stof(line[PEXINFO_INDEX::PEXINFO_INDEX_VALUE]);
-			
+
 			node1_lvl = line[PEXINFO_INDEX_NODE1_LVL];
 			node2_lvl = line[PEXINFO_INDEX_NODE2_LVL];
 
-			
+
 			if (node1_lvl == "RX") {
 				node1.x = node2.x;
 				node1.y = node2.y;
 			}
-			
+
 			description = line[PEXINFO_INDEX::PEXINFO_INDEX_DESCRIPTION];
 			PEXResDirection resistor_direction = PEXResistor::checkDirectionFromDescription(description);
-			if( value > 0 ) updateMinMaxRes(value, resistor_direction ); //임의의 코드, 0보다 작은 저항은 무시
-			this->resistors.push_back(PEXResistor(item_name, node1, node2, value, resistor_direction));				
+			if (value > 0) updateMinMaxRes(value, resistor_direction); //임의의 코드, 0보다 작은 저항은 무시
+			this->resistors.push_back(PEXResistor(item_name, node1, node2, value, resistor_direction));
 		}
 	}
 
@@ -176,15 +176,15 @@ namespace lve {
 			if (this->min_resistor_horizontal > new_value) this->min_resistor_horizontal = new_value;
 			if (this->max_resistor_horizontal < new_value) this->max_resistor_horizontal = new_value;
 		}
-		
+
 	}
 
 	void PEXResistorDataManager::printData() {
 		for (auto item : this->resistors) {
 			item.print();
 		}
-		printf("##Resistor Count : %lld, Vertical Res Max = %10.5f, Min = %10.5f, Horizontal Res Max = %10.5f, Min = %10.5f\n", 
-			this->resistors.size(), 
+		printf("##Resistor Count : %lld, Vertical Res Max = %10.5f, Min = %10.5f, Horizontal Res Max = %10.5f, Min = %10.5f\n",
+			this->resistors.size(),
 			this->getMaxResistorVerticalValue(), this->getMinResistorVerticalValue(),
 			this->getMaxResistorHorizontalValue(), this->getMinResistorHorizontalValue());
 	}
@@ -250,18 +250,18 @@ namespace lve {
 
 			node1_lvl = line[PEXINFO_INDEX_NODE1_LVL];
 			node2_lvl = line[PEXINFO_INDEX_NODE2_LVL];
-						
+
 			updateMinMaxCap(value);
-			this->capasitors.push_back(PEXCapacitor(item_name, node1, node2, value));			
+			this->capasitors.push_back(PEXCapacitor(item_name, node1, node2, value));
 		}
 	}
 
-	
+
 	inline void PEXCapacitorDataManager::updateMinMaxCap(const double& new_value) {
 		if (this->min_capacitor > new_value) this->min_capacitor = new_value;
 		if (this->max_capacitor < new_value) this->max_capacitor = new_value;
 	}
-	
+
 
 	void PEXCapacitorDataManager::printData() {
 
@@ -274,7 +274,7 @@ namespace lve {
 	}
 
 
-	void testPEXData(){
+	void testPEXData() {
 		PEXResistorDataManager resistor_data;
 		resistor_data.loadData("Data/MVP_PEX_RC_r1_0130_0208version.csv");
 		resistor_data.printData();
