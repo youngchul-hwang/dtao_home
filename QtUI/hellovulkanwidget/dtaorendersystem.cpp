840fda81b6855364cca838b185cd252de1bc758d
youngchul-hwang <89388094+youngchul-hwang@users.noreply.github.com>
2023-02-28 18:20:20 +0900

    Update dtaorendersystem.cpp

--- QtUI/hellovulkanwidget/dtaorendersystem.cpp
+++ QtUI/hellovulkanwidget/dtaorendersystem.cpp
@@ -1,7 +1,68 @@
+
 #include "dtaorendersystem.h"
 
-DtaoRenderSystem::DtaoRenderSystem()
+#include <QVulkanWindow>
+#include <QVulkanInstance>
+#include <vulkan/vulkan.h>
+
+DtaoRenderSystem::DtaoRenderSystem(LveWindow *w)
+    : lveWindow(w)
 {
+    qDebug() << "\n\n$$$$$ DtaoRenderSystem()";
+}
+
+DtaoRenderSystem::~DtaoRenderSystem()
+{
+    qDebug() << "\n\n$$$$$ ~DtaoRenderSystem()";
+    deleteLveDevice();
+    deleteLveRenderer();
+}
+
+void DtaoRenderSystem::startNextFrame(){
 
 }
 
+void DtaoRenderSystem::initResources() {
+    qDebug() << "\n\n$$$$$ DtaoRenderSystem::initResources()";
+    createLveDevice();
+    createLveRenderer();
+}
+
+void DtaoRenderSystem::releaseResources() {
+    qDebug() << "\n\n$$$$$ DtaoRenderSystem::releaseResources()";
+}
+
+void DtaoRenderSystem::initSwapChainResources() {
+    qDebug() << "\n\n$$$$$ DtaoRenderSystem::initSwapChainResources()";
+    QSize image_size = this->lveWindow->swapChainImageSize();
+    VkExtent2D extent;
+    extent.width = image_size.width();
+    extent.height = image_size.height();
+    this->lveRenderer->setExtent(extent);
+    this->lveRenderer->recreateSwapChain();
+}
+
+
+void DtaoRenderSystem::releaseSwapChainResources() {
+    qDebug() << "\n\n$$$$$ DtaoRenderSystem::releaseSwapChainResources()";
+}
+
+void DtaoRenderSystem::createLveDevice(){
+    qDebug() << "\n\n$$$$$ DtaoRenderSystem::creatLveDevice()";
+    this->lveDevice = new LveDevice(this->lveWindow, this->lveWindow->vulkanInstance());
+}
+
+void DtaoRenderSystem::deleteLveDevice(){
+    qDebug() << "\n\n$$$$$ DtaoRenderSystem::deleteLveDevice()";
+    delete this->lveDevice;
+}
+
+void DtaoRenderSystem::createLveRenderer(){
+    qDebug() << "\n\n$$$$$ DtaoRenderSystem::createLveRenderer()";
+    this->lveRenderer = new LveRenderer(*this->lveWindow, *this->lveDevice);
+}
+
+void DtaoRenderSystem::deleteLveRenderer(){
+    qDebug() << "\n\n$$$$$ DtaoRenderSystem::deleteLveRenderer()";
+    delete this->lveRenderer;
+}
